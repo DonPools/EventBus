cdf7f9f92804a457273cb163f5394bfcde843cc7
rocky <rocky@gmail.com>
2021-12-08 02:48:47 +0800

    Fix bug for closure.

--- event_bus.go
+++ event_bus.go
@@ -186,8 +186,7 @@ func (bus *EventBus) removeHandler(topic string, idx int) {
 func (bus *EventBus) findHandlerIdx(topic string, callback reflect.Value) int {
 	if _, ok := bus.handlers[topic]; ok {
 		for idx, handler := range bus.handlers[topic] {
-			if handler.callBack.Type() == callback.Type() &&
-				handler.callBack.Pointer() == callback.Pointer() {
+			if handler.callBack == callback {
 				return idx
 			}
 		}
